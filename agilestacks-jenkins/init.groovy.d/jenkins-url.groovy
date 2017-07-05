0bebdee40b448b677c1b51a7ec7eff52994787e7
Antons Kranga <anton@agilestacks.com>
2017-07-05 19:12:08 +0300

    introduced agilestacks-jenkins component

---
+++ agilestacks-jenkins/init.groovy.d/jenkins-url.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+import jenkins.model.*
+import java.util.logging.Logger
+
+def log = Logger.getLogger(this.class.name)
+
+// override jenkins
+def jlc = JenkinsLocationConfiguration.get()
+jlc.setUrl('${jenkins_url}') // changed during provisioning
+jlc.setAdminAddress('anton@agilestacks.com') // changed during provisioning
+log.info('Override Jenkins URL to ${jenkins_url}')
+jlc.save()
