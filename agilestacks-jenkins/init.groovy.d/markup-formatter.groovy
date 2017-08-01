40be39f890bba32dfc2b644413bc53ca7bcb5dbb
Antons Kranga <anton@agilestacks.com>
2017-08-01 17:25:22 +0300

    added utilitary scripts for jenkins

---
+++ agilestacks-jenkins/init.groovy.d/markup-formatter.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/env groovy
+
+import hudson.markup.RawHtmlMarkupFormatter
+
+Jenkins.instance.setMarkupFormatter(new RawHtmlMarkupFormatter(false))
+Jenkins.instance.save()
+
+println 'Markup formatter configured.'
