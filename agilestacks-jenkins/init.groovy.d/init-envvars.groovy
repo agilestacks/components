886d776de6175b04d4d8cf5fd2761ae7bdffc302
Antons Kranga <anton@agilestacks.com>
2017-08-01 17:25:07 +0300

    improved jenkins config

--- agilestacks-jenkins/init.groovy.d/init-envvars.groovy
+++ agilestacks-jenkins/init.groovy.d/init-envvars.groovy
@@ -2,43 +2,28 @@
 
 import jenkins.model.*
 import java.util.logging.Logger
+import java.io.*
 
 import io.fabric8.kubernetes.api.model.NamespaceBuilder
 import io.fabric8.kubernetes.client.*
 
-def kubeClient() {
-    return new DefaultKubernetesClient(new ConfigBuilder().withNamespace('automation-hub').build())
-}
 
-// def jsonFile  = kubeClient().
-//                   configMaps().
-//                   inNamespace('automation-hub').
-//                   withName('jenkins').get().data['cloud.json']
-// def slurper = new groovy.json.JsonSlurper()
-// def conf  = slurper.parseText(jsonFile)
+def namespace = new File("/var/run/secrets/kubernetes.io/serviceaccount/namespace").text
 
 def jenk = Jenkins.instance
 def globalProps = jenk.globalNodeProperties
 def envVarsNodePropertyList = globalProps.envVars
 
-//if (envVarsNodePropertyList == null) {
-//  envVarsNodePropertyList = new hudson.slaves.EnvironmentVariablesNodeProperty()
-//}
-
-if ( envVarsNodePropertyList.empty ) {
-  envVarsNodePropertyList << [:]
+if (envVarsNodePropertyList == null || envVarsNodePropertyList.empty) {
+ globalProps << new hudson.slaves.EnvironmentVariablesNodeProperty()
 }
-
 
 def envVars = envVarsNodePropertyList[0]
   
-def facts =  kubeClient().
+def facts =  new DefaultKubernetesClient(new ConfigBuilder().build()).
                   configMaps().
-                  inNamespace('jenkins').
+                  inNamespace(namespace).
                   withName('agilestacks').get().data
 
 envVars << facts
 jenk.save()
-
-//automationHubImage    = conf.automation_hub_image
-//automationHubEndpoint = conf.automation_hub_endpoint
