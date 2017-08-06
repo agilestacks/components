7526da1dc35c708acc5c6e4eff8b59e83d6e8667
Antons Kranga <anton@agilestacks.com>
2017-08-06 15:08:22 +0300

    improved slack configuration

--- agilestacks-jenkins/init.groovy.d/slack.groovy
+++ agilestacks-jenkins/init.groovy.d/slack.groovy
@@ -8,6 +8,8 @@ import io.fabric8.kubernetes.api.model.NamespaceBuilder
 import io.fabric8.kubernetes.client.*
 import com.cloudbees.plugins.credentials.domains.*
 import com.cloudbees.plugins.credentials.*
+import net.sf.json.JSONObject
+import jenkins.plugins.slack.*
 
 def namespace = new File("/var/run/secrets/kubernetes.io/serviceaccount/namespace").text
 
@@ -20,7 +22,9 @@ def conf =  new DefaultKubernetesClient(new ConfigBuilder().build()).
                   withName('agilestacks').get().data
 
 def log = Logger.getLogger(this.class.name)
-def store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+def store = Jenkins.instance.getExtensionList(
+  'com.cloudbees.plugins.credentials.SystemCredentialsProvider'
+)[0].getStore()
 def domain = Domain.global()
 
 def credentials = new org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl(
@@ -28,6 +32,26 @@ def credentials = new org.jenkinsci.plugins.plaincredentials.impl.StringCredenti
   'slack-secret',
   'Slack integration token',
   new hudson.util.Secret(conf.SLACK_TOKEN))
-//store.addCredentials(domain, credentials)
-//jenk.save()
+store.addCredentials(domain, credentials)
+
+def slack = Jenkins.instance.getExtensionList(
+  jenkins.plugins.slack.SlackNotifier.DescriptorImpl.class
+)[0]
+
+JSONObject formData = ['slack': ['tokenCredentialId': 'slack-secret']] as JSONObject
+
+def params = [
+  slackTeamDomain: conf.SLACK_TEAM,
+  slackToken: '',
+  slackRoom: conf.SLACK_ROOM,
+  slackBuildServerUrl: conf.JENKINS_URL,
+  slackSendAs: ''
+]
+def req = [
+  getParameter: { name -> params[name] }
+] as org.kohsuke.stapler.StaplerRequest
+slack.configure(req, formData)
+
+
+jenk.save()
 
