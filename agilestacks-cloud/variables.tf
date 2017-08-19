0d647d0921855d1a899cce1c6e1888db17612504
Antons Kranga <anton@agilestacks.com>
2017-08-19 10:46:12 +0300

    added cross-account role terraform

---
+++ agilestacks-cloud/variables.tf
@@ -0,0 +1,27 @@
+variable "external_id" {
+  type = "string"
+  description = "External ID to assume role"
+  default = ""
+}
+
+variable "assume_role_arn" {
+  type = "string"
+  description = "arn of the role to assume"
+  default = ""
+}
+
+variable "external_aws_region" {
+  type = "string"
+  description = "External account region"
+  default = "us-east-2"
+}
+
+variable "external_aws_access_key" {
+  type = "string"
+  default = ""
+}
+
+variable "external_aws_secret_key" {
+  type = "string"
+  default = ""
+}
