3c83653ab42e39b8c9c4ed6af361bc17a6669ffd
Antons Kranga <anton@agilestacks.com>
2017-08-21 18:00:15 +0300

    implemented ecr support for agiletacks-cloud

---
+++ agilestacks-cloud/terraform.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 0.9.3"
+  backend "s3" {}
+}
+
+provider "aws" {}
+
+variable "name" {
+  type = "string"
+  default = "dev"
+}
+
+variable "base_domain" {
+  type = "string"
+  default = "stacks.delivery"
+}
+
+
+module "ecr" {
+  source = "github.com/agilestacks/terraform-modules//ecr"
+  name   = "agilestacks/${var.name}.${var.base_domain}/cloud"
+}
+
+output "repository_url" {
+  value = "${module.ecr.repository_url}"
+}
+
+output "name" {
+  value = "${module.ecr.name}"
+}
+
+output "registry_id" {
+  value = "${module.ecr.registry_id}"
+}
