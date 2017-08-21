efda8fd5e41b01dcff20d69c6e9f84cbeb139404
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2017-08-21 18:04:50 +0300

    Outputs to match Hub

--- agilestacks-cloud/docker/output.tf
+++ agilestacks-cloud/docker/output.tf
@@ -1,19 +1,23 @@
-output "account_id" {
+output "aws_account_number" {
   value = "${data.aws_caller_identity.client.account_id}"
 }
 
-output "base_hosted_zone_id" {
+output "parent_domain_zone_id" {
   value = "${data.aws_route53_zone.base.zone_id}"
 }
 
-output "hosted_zone_id" {
+output "base_domain_zone_id" {
   value = "${aws_route53_zone.new.zone_id}"
 }
 
-output "s3_bucket" {
+output "s3_bucket_name" {
   value = "${aws_s3_bucket.main.bucket}"
 }
 
-output "domain_name" {
+output "s3_bucket_region" {
+  value = "${aws_s3_bucket.main.region}"
+}
+
+output "base_domain" {
   value = "${aws_route53_zone.new.name}"
 }
