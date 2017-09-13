6f1d534ae43a18d32f84250cc82ad1563b495a5d
Antons Kranga <anton@agilestacks.com>
2017-09-13 11:12:14 +0300

    introduced shared libraries

---
+++ jenkins/docker/groovy/shared-libs.groovy
@@ -0,0 +1,22 @@
+#!/usr/bin/env groovy
+import jenkins.model.*
+import org.jenkinsci.plugins.workflow.libs.*
+import hudson.plugins.filesystem_scm.*
+
+def sharedLibsHome = System.getenv('SHARED_LIBS') ?: '/shared-libs'
+
+def oldLibs = GlobalLibraries.get().libraries
+def newLibs = (new File(sharedLibsHome).
+  listFiles().findAll { file ->
+    file.directory && oldLibs.find {file.name == it.name} == null
+  }.collect { dir ->
+    println "Register shared library: ${dir.name}@master from ${dir.path}"
+    def scm = new FSSCM(dir.path, false, false, null)
+    def lib = new LibraryConfiguration(dir.name, new SCMRetriever(scm))
+    lib.defaultVersion = 'master'
+    lib.implicit = false
+    lib.allowVersionOverride = true
+    lib
+  }
+)
+GlobalLibraries.get().libraries = (oldLibs + newLibs)
