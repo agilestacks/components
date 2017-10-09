62cf8c891ded792b796f9b8c48f43d187eeb16e5
Rick Richardson <rick@quiver.zone>
2017-10-09 14:35:27 -0700

    SAML auto-sign-in working

--- gitlab-ce/replace.sh
+++ gitlab-ce/replace.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -e
+#!/bin/bash -x
 
 # Dirty ugly hack for inserting data into configmap :-( :-[ :-<
 sso_url=$(cat okta/ssoUrl)
-sed -i '' "s/#SAML_TARGET_URL#/https://$sso_url" values.yaml
+sed -i '' "s|#SAML_TARGET_URL#|$sso_url|g" values.yaml
 
-fingerprint=$(openssl x509 -in okta/okta.pem -inform pem -noout -fingerprint)
-sed -i '' "s/#SAML_CERT_FINGERPRINT#/$fingerprint" values.yaml
+fingerprint=$(openssl x509 -in okta/okta.pem -inform pem -noout -fingerprint | cut -d'=' -f2)
+sed -i '' "s|#SAML_CERT_FINGERPRINT#|$fingerprint|g" values.yaml
 
 
