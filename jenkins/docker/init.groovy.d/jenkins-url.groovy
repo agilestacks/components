1e7e0e6e28fe358bb06e6706b0096467ca161e22
Antons Kranga <anton@agilestacks.com>
2017-10-10 09:43:40 +0300

    introduced shared libs

---
+++ jenkins/docker/init.groovy.d/jenkins-url.groovy
@@ -0,0 +1,15 @@
+#!/usr/bin/env groovy
+
+import jenkins.model.*
+import java.util.logging.Logger
+
+def log   = Logger.getLogger(this.class.name)
+def url   = System.getenv('JENKINS_URL')
+def email = System.getenv('ADMIN_EMAIL')
+
+// override jenkins
+def jlc = JenkinsLocationConfiguration.get()
+jlc.setUrl( url )
+jlc.setAdminAddress(email) 
+log.info("Override Jenkins URL to $url")
+jlc.save()
---
+++ jenkins/docker/shared-libs/vars/withEcr.groovy
@@ -0,0 +1,29 @@
+#!/usr/bin/env groovy
+
+@Grab(group = 'com.amazonaws', module = 'aws-java-sdk', version = '1.11.119')
+import com.amazonaws.services.ecr.AmazonECRClientBuilder
+import com.amazonaws.services.ecr.model.GetAuthorizationTokenRequest
+import com.amazonaws.auth.InstanceProfileCredentialsProvider
+import com.amazonaws.regions.Regions
+
+def call(params = [:], Closure body) {
+    def region = params?.region ?: Regions.currentRegion.name
+
+    def token = AmazonECRClientBuilder.
+            standard().
+            withCredentials(InstanceProfileCredentialsProvider.getInstance()).
+            withRegion(region).
+            build().
+            getAuthorizationToken(new GetAuthorizationTokenRequest())
+    def auth = token.authorizationData[0]
+    def login = new String(auth.authorizationToken.decodeBase64()).tokenize(':')
+
+    def result = sh  script: "docker login -u ${login[0]} -p ${login[1]} ${auth.proxyEndpoint}",
+                     returnStatus:true
+
+    echo "docker login result: ${result}"
+
+    if (body != null) {
+        body()
+    }
+}
