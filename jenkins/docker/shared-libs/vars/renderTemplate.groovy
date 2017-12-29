d034ccc81978a8a12da641a157df0d96d2211293
Antons Kranga <anton@agilestacks.com>
2017-12-29 12:31:45 +0200

    updated jenkins to the latest LTS

--- jenkins/docker/shared-libs/vars/renderTemplate.groovy
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(template, bindings = [:]) {
-    return new groovy.text.SimpleTemplateEngine()
-        .createTemplate(template)
-        .make(bindings)
-        .toString()
-}
