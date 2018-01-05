65105f12a7f271568a5955826dcd17861c15a0f8
Rick Richardson <rick@quiver.zone>
2018-01-04 23:27:31 -0800

    added waits for gitlab pod startup and proper response from login,  also a new curl approach so we don't have to resolve DNS

--- gitlab-ce/replace.sh
+++ gitlab-ce/replace.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
 
-# Dirty ugly hack for inserting data into configmap :-( :-[ :-<
-sso_url=$(cat okta/ssoUrl)
-sed -i '' "s|#SAML_TARGET_URL#|$sso_url|g" values.yaml
+# Dirty ugly hack for inserting data into values.yaml
+sed -i '' "s|#GITLAB_ROOT_PASSWORD#|$GITLAB_ROOT_PASSWORD|g" values.yaml
+	
+	#sso_url=$(cat okta/ssoUrl)
+#sed -i '' "s|#SAML_TARGET_URL#|$sso_url|g" values.yaml
 
-fingerprint=$(openssl x509 -in okta/okta.pem -inform pem -noout -fingerprint | cut -d'=' -f2)
-sed -i '' "s|#SAML_CERT_FINGERPRINT#|$fingerprint|g" values.yaml
+#fingerprint=$(openssl x509 -in okta/okta.pem -inform pem -noout -fingerprint | cut -d'=' -f2)
+#sed -i '' "s|#SAML_CERT_FINGERPRINT#|$fingerprint|g" values.yaml
 
 
