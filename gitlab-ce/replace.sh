99ec21f2ad5526962629907081cd6ac4e5cc702e
Artem Kudla <kudla.artem@gmail.com>
2018-01-09 22:23:14 +0200

    gitlab values usin both darwin and linux compatible notation for sed

--- gitlab-ce/replace.sh
+++ gitlab-ce/replace.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -x
 
 # Dirty ugly hack for inserting data into values.yaml
-sed -i '' "s|#GITLAB_ROOT_PASSWORD#|$GITLAB_ROOT_PASSWORD|g" values.yaml
-	
+
+sed -i -- "s|#GITLAB_ROOT_PASSWORD#|$GITLAB_ROOT_PASSWORD|g" values.yaml
+
 	#sso_url=$(cat okta/ssoUrl)
-#sed -i '' "s|#SAML_TARGET_URL#|$sso_url|g" values.yaml
+#sed -i -- "s|#SAML_TARGET_URL#|$sso_url|g" values.yaml
 
 #fingerprint=$(openssl x509 -in okta/okta.pem -inform pem -noout -fingerprint | cut -d'=' -f2)
-#sed -i '' "s|#SAML_CERT_FINGERPRINT#|$fingerprint|g" values.yaml
-
-
+#sed -i -- "s|#SAML_CERT_FINGERPRINT#|$fingerprint|g" values.yaml
