c16d251a45e02727fdbb89a1583ce245e6934509
Arkadi Shishlov <arkadi@agilestacks.com>
2018-03-02 18:23:11 +0200

    Explicit context

--- hello-web/deploy.sh
+++ hello-web/deploy.sh
@@ -3,6 +3,7 @@
 echo Deploying Hello Web on \`$NAME\` stack in \`$DOMAIN_NAME\` domain
 
 name=hello-web-$NAME
-kubectl run $name --image=gcr.io/google-samples/hello-app:1.0 --port=8080
-kubectl expose deployment $name --type=ClusterIP
-kubectl apply -f ingress.yaml
+kubectl="kubectl --context=$DOMAIN_NAME"
+$kubectl run $name --image=gcr.io/google-samples/hello-app:1.0 --port=8080
+$kubectl expose deployment $name --type=ClusterIP
+$kubectl apply -f ingress.yaml
