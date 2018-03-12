6e02e65102a2ffee9348ec5d2e37148deefcf27f
Oleg Shepetyuk <oleg@agilestacks.com>
2018-03-12 21:20:48 +0200

    Switched nginxing component to terraform, updated gitignore

---
+++ nginxing/terraform-aws.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_version = ">= 0.11.0"
+  backend "s3" {}
+}
+
+provider "aws" {
+  version = "1.10.0"
+}
+
+provider "kubernetes" {
+  version = "1.0.1"
+  config_context_auth_info = "admin@${var.domain_name}"
+  config_context_cluster   = "${var.domain_name}"
+}
+
+data "aws_route53_zone" "ext_zone" {
+  name  = "${var.domain_name}"
+}
+
+data "kubernetes_service" "nginxing" {
+  metadata {
+    name      = "${var.component}-nginx-ingress-controller"
+    namespace = "${var.namespace}"
+  }
+}
+
+resource "aws_route53_record" "dns_url" {
+  zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
+  name    = "${var.url_prefix}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${data.kubernetes_service.nginxing.load_balancer_ingress.0.hostname}"]
+
+  lifecycle {
+    ignore_changes = ["records", "ttl"]
+  }
+}
+
+resource "aws_route53_record" "dns_url_wildcard" {
+  zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
+  name    = "*.${var.url_prefix}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${data.kubernetes_service.nginxing.load_balancer_ingress.0.hostname}"]
+
+  lifecycle {
+    ignore_changes = ["records", "ttl"]
+  }
+}
+
