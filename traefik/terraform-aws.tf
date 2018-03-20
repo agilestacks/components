b2c5ff513da53d8178b03996c373d611a7a5d2c0
Oleg Shepetyuk <olegshepetjuk@gmail.com>
2018-03-20 14:58:23 +0200

    Added private DNS records, for app/apps for in-cluster usage

--- traefik/terraform-aws.tf
+++ traefik/terraform-aws.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 0.11.0"
-  backend "s3" {}
+  backend          "s3"             {}
 }
 
 provider "aws" {
@@ -8,17 +8,17 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  version = "1.0.1"
+  version                  = "1.0.1"
   config_context_auth_info = "admin@${var.domain_name}"
   config_context_cluster   = "${var.domain_name}"
 }
 
 data "aws_route53_zone" "ext_zone" {
-  name  = "${var.domain_name}"
+  name = "${var.domain_name}"
 }
 
 data "aws_route53_zone" "int_zone" {
-  name  = "${var.domain_name}"
+  name         = "${var.domain_name}"
   private_zone = true
 }
 
@@ -26,18 +26,6 @@ data "kubernetes_service" "traefik" {
   metadata {
     name      = "${var.component}"
     namespace = "${var.namespace}"
-  }
-}
-
-resource "aws_route53_record" "dns_auth_int" {
-  zone_id = "${data.aws_route53_zone.int_zone.zone_id}"
-  name    = "${var.auth_url_prefix}"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.hostname}"]
-
-  lifecycle {
-    ignore_changes = ["records", "ttl"]
   }
 }
 
@@ -53,7 +41,7 @@ resource "aws_route53_record" "dns_auth_ext" {
   }
 }
 
-resource "aws_route53_record" "dns_app1" {
+resource "aws_route53_record" "dns_app1_ext" {
   zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
   name    = "${var.url_prefix}"
   type    = "CNAME"
@@ -65,7 +53,7 @@ resource "aws_route53_record" "dns_app1" {
   }
 }
 
-resource "aws_route53_record" "dns_app2" {
+resource "aws_route53_record" "dns_app2_ext" {
   zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
   name    = "*.${var.url_prefix}"
   type    = "CNAME"
@@ -77,7 +65,7 @@ resource "aws_route53_record" "dns_app2" {
   }
 }
 
-resource "aws_route53_record" "dns_apps1" {
+resource "aws_route53_record" "dns_apps1_ext" {
   zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
   name    = "${var.sso_url_prefix}"
   type    = "CNAME"
@@ -89,7 +77,7 @@ resource "aws_route53_record" "dns_apps1" {
   }
 }
 
-resource "aws_route53_record" "dns_apps2" {
+resource "aws_route53_record" "dns_apps2_ext" {
   zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
   name    = "*.${var.sso_url_prefix}"
   type    = "CNAME"
@@ -101,3 +89,63 @@ resource "aws_route53_record" "dns_apps2" {
   }
 }
 
+# Private records, for in-cluster usage
+resource "aws_route53_record" "dns_auth_int" {
+  zone_id = "${data.aws_route53_zone.int_zone.zone_id}"
+  name    = "${var.auth_url_prefix}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.hostname}"]
+
+  lifecycle {
+    ignore_changes = ["records", "ttl"]
+  }
+}
+
+resource "aws_route53_record" "dns_app1_ext" {
+  zone_id = "${data.aws_route53_zone.int_zone.zone_id}"
+  name    = "${var.url_prefix}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.hostname}"]
+
+  lifecycle {
+    ignore_changes = ["records", "ttl"]
+  }
+}
+
+resource "aws_route53_record" "dns_app2_ext" {
+  zone_id = "${data.aws_route53_zone.int_zone.zone_id}"
+  name    = "*.${var.url_prefix}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.hostname}"]
+
+  lifecycle {
+    ignore_changes = ["records", "ttl"]
+  }
+}
+
+resource "aws_route53_record" "dns_apps1_ext" {
+  zone_id = "${data.aws_route53_zone.int_zone.zone_id}"
+  name    = "${var.sso_url_prefix}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.hostname}"]
+
+  lifecycle {
+    ignore_changes = ["records", "ttl"]
+  }
+}
+
+resource "aws_route53_record" "dns_apps2_ext" {
+  zone_id = "${data.aws_route53_zone.int_zone.zone_id}"
+  name    = "*.${var.sso_url_prefix}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.hostname}"]
+
+  lifecycle {
+    ignore_changes = ["records", "ttl"]
+  }
+}
--- traefik/terraform-out.tf
+++ traefik/terraform-out.tf
@@ -1,9 +1,9 @@
 output "ingress_fqdn" {
-  value = "${aws_route53_record.dns_app1.fqdn}"
+  value = "${aws_route53_record.dns_app1_ext.fqdn}"
 }
 
 output "sso_ingress_fqdn" {
-  value = "${aws_route53_record.dns_apps1.fqdn}"
+  value = "${aws_route53_record.dns_apps1_ext.fqdn}"
 }
 
 output "elb_domain" {
