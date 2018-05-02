279ec25a9727c034700907ec98a9670b6561b72f
Artem Kudla <artem@agilestacks.com>
2018-05-02 13:03:06 +0300

    mongo dashboard wait refactored

--- mongodb-web/docker-wait-for-port/entrypoint.sh
+++ mongodb-web/docker-wait-for-port/entrypoint.sh
@@ -1,18 +1,20 @@
 #!/bin/sh -e
+PORT=${PORT:-$DEFAULT_PORT}
+if test -n "$WAIT_TIMEOUT" && test -n "$HOST" && test -n "$PORT"; then
+	PORT_NAME=${PORT_NAME:-"$HOST:$PORT"}
+	EXPIRE=$(echo "`date +%s` + $WAIT_TIMEOUT" | bc)
+        CHECK_INTERVAL=${CHECK_INTERVAL:-8}
 
-if test -n "$WAIT_TIMEOUT"; then
-    EXPIRE=$(echo "`date +%s` + $WAIT_TIMEOUT" | bc)
+	printf "Waiting for $PORT_NAME to be served "
 
-    printf "Waiting for mongodb port to be served "
-
-    while [ `date +%s` -le "$EXPIRE" ]; do
-            if nc -zw 1 $DB_HOST $DB_PORT >& /dev/null; then
-                    printf "\ndone\n"
-                    exit 0
-            fi
-            printf "."
-            sleep 8
-    done
-    printf "\nERROR timeout $WAIT_TIMEOUT sec\n"
-    exit 1
+	while [ `date +%s` -le "$EXPIRE" ]; do
+			if nc -zw 1 $HOST $PORT >& /dev/null; then
+					printf "\ndone\n"
+					exit 0
+			fi
+			printf "."
+			sleep $CHECK_INTERVAL
+	done
+	printf "\nERROR timeout $WAIT_TIMEOUT sec\n"
+	exit 1
 fi
