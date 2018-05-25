c3e4bde390c5f96f4bb178adccb0a9562f9ea5be
Antons Kranga <anton@agilestacks.com>
2018-05-25 13:59:07 +0300

    introduced sg clean up script (resolve agilestacks/stack-k8s-aws#122)

--- traefik/terraform-aws.tf
+++ traefik/terraform-aws.tf
@@ -89,12 +89,44 @@ resource "aws_route53_record" "dns_apps2_ext" {
 resource "null_resource" "drop_elb" {
   provisioner "local-exec" {
     when = "destroy"
-    on_failure = "continue"
+    on_failure = "fail"
     command = <<EOF
+export AWS_DEFAULT_REGION="${data.aws_region.current.name}"
+ELB_NAME="${element(split("-", element(split(".", "${data.kubernetes_service.traefik.load_balancer_ingress.0.hostname}"), 0)), 0)}"
+SG_IDS=$(aws \
+  elb describe-load-balancers \
+  --load-balancer-names $ELB_NAME --query 'LoadBalancerDescriptions[*].SecurityGroups[*]' --output=text \
+  | xargs)
+
+echo "Delete ELB $ELB_NAME"
 aws \
-  --region=${data.aws_region.current.name} \
   elb delete-load-balancer  \
-  --load-balancer-name="${element(split("-", element(split(".", "${data.kubernetes_service.traefik.load_balancer_ingress.0.hostname}"), 0)), 0)}"
+  --load-balancer-name=$ELB_NAME
+
+for ID in "$SG_IDS"; do
+  echo "Proceed with SG cleanup: $ID"
+  SG_REVOKES=$(aws \
+      ec2 describe-security-groups \
+      --query "SecurityGroups[?contains(IpPermissions[].UserIdGroupPairs[].GroupId,'$ID')].GroupId" \
+      --output=text | xargs )
+  echo "Revoking ingress from: $SG_REVOKES"
+  for R in "$SG_REVOKES"; do
+    echo "Proceed revoke $R ingress route source: $ID"
+    aws \
+      ec2 revoke-security-group-ingress \
+      --group-id "$R" --source-group "$ID" --protocol all;
+  done;
+
+  echo "Delete SG $ID"
+  for n in $(seq 10); do
+    aws \
+      ec2 delete-security-group \
+      --group-id="$ID" \
+    && break;
+    echo "Retry $n in 10sec..."
+    sleep 10
+  done
+done
 EOF
 
   }
