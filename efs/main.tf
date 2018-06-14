7e48d63b94755e8ff2e8d63ebe36ce59aa6c3ad4
Oleg Shepetyuk <olegshepetjuk@gmail.com>
2018-06-14 19:43:03 +0300

    Bumped terraform aws provider version till 1.23.0

--- efs/main.tf
+++ efs/main.tf
@@ -1,14 +1,13 @@
 terraform {
   required_version = ">= 0.11.3"
-  backend "s3" {}
+  backend          "s3"             {}
 }
 
 provider "aws" {
-  version = "1.13.0"
+  version = "1.23.0"
 }
 
 data "aws_region" "current" {}
-
 
 # variable "aws_az" {
 #   type = "string"
@@ -19,7 +18,7 @@ variable "name" {
 }
 
 variable "performance_mode" {
-  type = "string"
+  type    = "string"
   default = "generalPurpose"
 }
 
@@ -36,19 +35,20 @@ variable "cname_zone" {
 }
 
 variable "cname_record" {
-  type = "string"
+  type    = "string"
   default = "nfs"
 }
-
 
 resource "aws_efs_file_system" "main" {
   # do we really need to replace `.` with `-`?
   # https://docs.aws.amazon.com/efs/latest/ug/API_CreateFileSystem.html#efs-CreateFileSystem-request-CreationToken
   # creation_token = "${replace("${var.name}", ".", "-")}"
   creation_token = "${var.name}"
+
   tags {
     Name = "${var.name}"
   }
+
   performance_mode = "${var.performance_mode}"
 }
 
@@ -63,7 +63,6 @@ locals {
   dns = "${aws_efs_file_system.main.id}.efs.${data.aws_region.current.name}.amazonaws.com"
 }
 
-
 data "aws_route53_zone" "int_zone" {
   name         = "${var.cname_zone}"
   private_zone = true
@@ -76,7 +75,6 @@ resource "aws_route53_record" "nfs_cname" {
   records = ["${local.dns}"]
   ttl     = "30"
 }
-
 
 output "efs_endpoint" {
   value = "${local.dns}"
--- traefik/terraform-aws.tf
+++ traefik/terraform-aws.tf
@@ -4,7 +4,7 @@ terraform {
 }
 
 provider "aws" {
-  version = "1.13.0"
+  version = "1.23.0"
 }
 
 provider "kubernetes" {
