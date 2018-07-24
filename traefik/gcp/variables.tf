291261937a1950faaa6f1ad180fecfaf15dacc8c
Arkadi Shishlov <arkadi@agilestacks.com>
2018-07-24 13:12:51 +0300

    GCP Traefik

---
+++ traefik/gcp/variables.tf
@@ -0,0 +1,32 @@
+variable "name" {
+  type = "string"
+  description = "Google DNS managed zone name"
+}
+
+variable "domain_name" {
+  type = "string"
+  description = "Domain name associated with Google DNS managed zone"
+}
+
+variable "namespace" {
+  type = "string"
+  default = "ingress"
+}
+
+variable "url_prefix" {
+  type = "string"
+}
+
+variable "sso_url_prefix" {
+  type = "string"
+}
+
+variable "auth_url_prefix" {
+  type = "string"
+  default = "auth"
+}
+
+variable "component" {
+  type = "string"
+  default = "traefik"
+}
