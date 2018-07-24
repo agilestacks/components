7db1f5dd11517d1be91c48e60e30ddc29c8814ae
Arkadi Shishlov <arkadi@agilestacks.com>
2018-07-24 13:17:46 +0300

    Better GKE adapter

--- k8s-gke/deploy.sh
+++ k8s-gke/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -xe
 
-DOMAIN_NAME=${TF_VAR_name}.${TF_VAR_base_domain}
-base_domain_zone=kubernetes
+name=$(echo $DOMAIN_NAME | cut -d. -f1)
+base_domain=$(echo $DOMAIN_NAME | cut -d. -f2-)
+
+base_domain_zone=agilestacks # this is the name of root zone in Google DNS, this become Cloud Account domain
 
 if ! kubectl config get-contexts $DOMAIN_NAME; then
     if test -z "$GKE_CLUSTER"; then
@@ -12,6 +14,13 @@ if ! kubectl config get-contexts $DOMAIN_NAME; then
     kubectl config rename-context $(kubectl config current-context) $DOMAIN_NAME
 fi
 
+describe=$(gcloud container clusters describe $GKE_CLUSTER --zone $GCP_ZONE --format json)
+endpoint=$(echo $describe | jq -r .endpoint)
+auth=$(echo $describe | jq -r .masterAuth)
+caCert=$(echo $auth | jq -r .clusterCaCertificate)
+clientCert=$(echo $auth | jq -r .clientCertificate)
+clientKey=$(echo $auth | jq -r .clientKey)
+
 BUCKET=gs://files-$(echo $DOMAIN_NAME | sed -e 's/\./-/g')
 
 if ! gsutil ls $BUCKET; then
@@ -19,37 +28,33 @@ if ! gsutil ls $BUCKET; then
 fi
 
 if ! gcloud dns managed-zones list --filter dnsName:$DOMAIN_NAME | grep -F $DOMAIN_NAME; then
-    if ! gcloud dns managed-zones list --filter dnsName:$TF_VAR_base_domain | grep -F $TF_VAR_base_domain; then
-        echo $TF_VAR_base_domain not found in Cloud DNS
+    if ! gcloud dns managed-zones list --filter dnsName:$base_domain | grep -F $base_domain; then
+        echo $base_domain not found in Cloud DNS
         exit 1
     fi
-    gcloud dns managed-zones create $TF_VAR_name --dns-name=$DOMAIN_NAME --description=$TF_VAR_name
+    gcloud dns managed-zones create $name --dns-name=$DOMAIN_NAME --description=$name
 
     rm -f transaction.yaml
-    gcloud dns record-sets transaction start -z $TF_VAR_name
-    gcloud dns record-sets transaction add -z $TF_VAR_name --name $DOMAIN_NAME --type A --ttl 300 \
-        $(gcloud container clusters describe cluster-1 --zone $GCP_ZONE --format json | jq -r .endpoint)
-    gcloud dns record-sets transaction execute -z $TF_VAR_name
+    gcloud dns record-sets transaction start -z $name
+    gcloud dns record-sets transaction add -z $name --name api.$DOMAIN_NAME --type A --ttl 300 $endpoint
+    gcloud dns record-sets transaction execute -z $name
 
     gcloud dns record-sets transaction start -z $base_domain_zone
     gcloud dns record-sets transaction add -z $base_domain_zone --name $DOMAIN_NAME --type NS --ttl 300 \
-        $(gcloud dns managed-zones describe ${TF_VAR_name} --format json | jq -r .nameServers[])
+        $(gcloud dns managed-zones describe ${name} --format json | jq -r .nameServers[])
     gcloud dns record-sets transaction execute -z $base_domain_zone
 fi
-
-auth=$(gcloud container clusters describe cluster-1 --zone $GCP_ZONE --format json | jq -r .masterAuth)
-caCert=$(echo $auth | jq -r .clusterCaCertificate)
-clientCert=$(echo $auth | jq -r .clientCertificate)
-clientKey=$(echo $auth | jq -r .clientKey)
 
 set +x
 
 echo Outputs:
 echo
-echo base_domain = $DOMAIN_NAME
+echo dns_name = $name
+echo dns_base_domain = $base_domain
 echo gcr_repository = gcr.io/$GCP_PROJECT
-echo gcp_bucket = $BUCKET
+echo gcs_bucket = $BUCKET
 echo api_ca_crt = $caCert
 echo api_client_crt = $clientCert
 echo api_client_key = $clientKey
+echo api_endpoint = api.$DOMAIN_NAME
 echo
