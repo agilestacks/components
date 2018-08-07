48260929d819528e1221967b94095b5ff981e8b1
Victor Oginskis <viktorsoginskis@gmail.com>
2018-08-07 15:33:13 +0300

    provisioning of separate role & policy for nodes  added

--- k8s-worker-nodes/main.tf
+++ k8s-worker-nodes/main.tf
@@ -13,10 +13,10 @@ data "ignition_config" "main" {
   }
 }
 
-data "aws_iam_role" "worker_role" {
-  count = "${var.worker_iam_role == "" ? 0 : 1}"
-  name  = "${var.worker_iam_role}"
-}
+# data "aws_iam_role" "worker_role" {
+#   count = "${var.worker_iam_role == "" ? 0 : 1}"
+#   name  = "${var.worker_iam_role}"
+# }
 
 data "aws_ami" "coreos_ami" {
   filter {
@@ -99,5 +99,95 @@ resource "aws_autoscaling_attachment" "workers" {
 
 resource "aws_iam_instance_profile" "worker_profile" {
   name = "worker-profile-${var.node_pool_name}-${var.base_domain}"
-  role = "${join("|", data.aws_iam_role.worker_role.*.name)}"
+  role = "${aws_iam_role.worker_role.name}"
+}
+
+resource "aws_iam_role" "worker_role" {
+  name = "worker-role-${var.node_pool_name}-${var.base_domain}"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": { "Service": "ec2.amazonaws.com"},
+      "Action": "sts:AssumeRole"
+    },
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": { "Service": "ecs-tasks.amazonaws.com"}
+    },
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": { "Service": "batch.amazonaws.com"}
+    },
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {"AWS": "*"}
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "worker_policy" {
+  name = "worker_policy-${var.node_pool_name}-${var.base_domain}"
+  role = "${aws_iam_role.worker_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:*",
+        "elasticloadbalancing:*",
+        "route53:*",
+        "s3:*",
+        "sts:*",
+        "dynamodb:*"
+      ],
+      "Resource": ["*"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:CompleteLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:InitiateLayerUpload",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetRepositoryPolicy",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:BatchGetImage",
+        "ecs:CreateCluster",
+        "ecs:DeregisterContainerInstance",
+        "ecs:DiscoverPollEndpoint",
+        "ecs:Poll",
+        "ecs:RegisterContainerInstance",
+        "ecs:StartTelemetrySession",
+        "ecs:Submit*",
+        "sts:AssumeRole"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action" : [
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeAutoScalingInstances"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
