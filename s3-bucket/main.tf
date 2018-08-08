cfb7cb88835708575976a570efe2ad3eef1e2483
Antons Kranga <anton@agilestacks.com>
2018-08-08 17:27:05 +0300

    More s3-bucket output parameters

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -41,3 +41,15 @@ resource "aws_s3_bucket" "main" {
 output "bucket" {
   value = "${aws_s3_bucket.main.bucket}"
 }
+
+output "arn" {
+  value = "${aws_s3_bucket.main.arn}"
+}
+
+output "domain" {
+  value = "${aws_s3_bucket.main.bucket_domain_name}"
+}
+
+output "region" {
+  value = "${aws_s3_bucket.main.region}"
+}
