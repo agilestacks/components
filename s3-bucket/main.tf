7e2d23bf0025eef5757f3047d3e3ef0c4ae499b1
Antons Kranga <anton@agilestacks.com>
2018-08-09 15:41:54 +0300

    S3 bucket exports service endpoint

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -3,6 +3,35 @@ terraform {
   backend "s3" {}
 }
 
+variable "endpoints" {
+  type    = "map"
+  description = "S3 service endpoints by region"
+  default = {
+    us-east-1 = "s3.amazonaws.com"
+    us-east-2 = "s3-us-east-2.amazonaws.com"
+    us-west-2 = "s3-us-west-2.amazonaws.com"
+    us-west-1 = "s3-us-west-1.amazonaws.com"
+    ca-central-1 = "s3-ca-central-1.amazonaws.com"
+    eu-west-1 = "s3-eu-west-1.amazonaws.com"
+    eu-west-2 = "s3-eu-west-2.amazonaws.com"
+    eu-west-3 = "s3-eu-west-3.amazonaws.com"
+    eu-central-1 = "s3-eu-central-1.amazonaws.com"
+    ap-south-1 = "s3-ap-south-1.amazonaws.com"
+    ap-southeast-1 = "s3-ap-southeast-1.amazonaws.com"
+    ap-southeast-2 = "s3-ap-southeast-2.amazonaws.com"
+    ap-northeast-1 = "s3-ap-northeast-1.amazonaws.com"
+    ap-northeast-2 = "s3-ap-northeast-2.amazonaws.com"
+    sa-east-1 = "s3-sa-east-1.amazonaws.com"
+    us-gov-west-1 = "s3-us-gov-west-1.amazonaws.com"
+    cn-north-1 = "s3.cn-north-1.amazonaws.com.cn"
+    cn-northwest-1 = "s3.cn-northwest-1.amazonaws.com.cn"
+  }
+}
+
+data "aws_region" "current" {
+  current = true
+}
+
 provider "aws" {
   version = "~> 1.25"
 }
@@ -36,6 +65,10 @@ resource "aws_s3_bucket" "main" {
 #       target_bucket = "${aws_s3_bucket.log_bucket.id}"
 #       target_prefix = "log/"
 #    }
+}
+
+output "endpoint" {
+  value = "${var.endpoints[ data.aws_region.current.name ]}"
 }
 
 output "bucket" {
