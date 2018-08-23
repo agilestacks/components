e789b6b13073ca35161ddd0d038e0b1d60ff9c77
Arkadi Shishlov <arkadi@agilestacks.com>
2018-08-23 16:29:19 +0300

    Traefik Hybrid target is AWS + Bare-metal

---
+++ traefik/hybrid/main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_version = ">= 0.11.3"
+  backend "s3" {}
+}
+
+provider "aws" {
+  version = "1.32.0"
+}
+
+data "aws_region" "current" {}
+
+data "aws_route53_zone" "ext_zone" {
+  name = "${var.domain_name}"
+}
+
+resource "aws_route53_record" "dns_auth_ext" {
+  zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
+  name    = "${var.auth_url_prefix}"
+  type    = "A"
+  ttl     = "300"
+  records = ["${var.ingress_static_ip}"]
+
+  lifecycle {
+    ignore_changes = ["records", "ttl"]
+  }
+}
+
+resource "aws_route53_record" "dns_app1_ext" {
+  zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
+  name    = "${var.url_prefix}"
+  type    = "A"
+  ttl     = "300"
+  records = ["${var.ingress_static_ip}"]
+
+  lifecycle {
+    ignore_changes = ["records", "ttl"]
+  }
+}
+
+resource "aws_route53_record" "dns_app2_ext" {
+  zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
+  name    = "*.${var.url_prefix}"
+  type    = "A"
+  ttl     = "300"
+  records = ["${var.ingress_static_ip}"]
+
+  lifecycle {
+    ignore_changes = ["records", "ttl"]
+  }
+}
+
+resource "aws_route53_record" "dns_apps1_ext" {
+  zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
+  name    = "${var.sso_url_prefix}"
+  type    = "A"
+  ttl     = "300"
+  records = ["${var.ingress_static_ip}"]
+
+  lifecycle {
+    ignore_changes = ["records", "ttl"]
+  }
+}
+
+resource "aws_route53_record" "dns_apps2_ext" {
+  zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
+  name    = "*.${var.sso_url_prefix}"
+  type    = "A"
+  ttl     = "300"
+  records = ["${var.ingress_static_ip}"]
+
+  lifecycle {
+    ignore_changes = ["records", "ttl"]
+  }
+}
---
+++ traefik/hybrid/variables.tf
@@ -0,0 +1,26 @@
+variable "domain_name" {
+  type = "string"
+  description = "Domain name associated with R53 hosted zone"
+}
+
+variable "namespace" {
+  type = "string"
+  default = "ingress"
+}
+
+variable "url_prefix" {
+  type = "string"
+}
+
+variable "sso_url_prefix" {
+  type = "string"
+}
+
+variable "auth_url_prefix" {
+  type = "string"
+  default = "auth"
+}
+
+variable "ingress_static_ip" {
+  type = "string"
+}
