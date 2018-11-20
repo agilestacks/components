ff3fd450736c5ba0b205e250a5316801368179ef
Arkadi Shishlov <arkadi@agilestacks.com>
2018-11-21 00:04:54 +0200

    PostgreSQL RDS Terraform v0.11, phonies, formatting (backup still on v0.9)

--- postgresql-rds/main.tf
+++ postgresql-rds/main.tf
@@ -1,9 +1,11 @@
 terraform {
-  required_version = ">= 0.9.3"
-  backend          "s3"             {}
+  required_version = ">= 0.11.3"
+  backend "s3" {}
 }
 
-provider "aws" {}
+provider "aws" {
+  version = "1.41.0"
+}
 
 data "aws_vpc" "selected" {
   id = "${var.vpc_id}"
