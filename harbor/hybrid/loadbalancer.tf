edb4f4fdda2da9a5d9a2a9aec6908b9be8311ce6
Antons Kranga <anton@agilestacks.com>
2018-12-17 16:25:43 -0800

    Cleaning up terraform parameters

--- harbor/hybrid/loadbalancer.tf
+++ harbor/hybrid/loadbalancer.tf
@@ -8,7 +8,7 @@ variable "ingress_static_ip" {
   type = "string"
 }
 
-resource "aws_route53_record" "dns_auth_ext" {
+resource "aws_route53_record" "dns_app_ext" {
   zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
   name    = "${var.component}.${var.service_prefix}"
   type    = "A"
@@ -16,14 +16,6 @@ resource "aws_route53_record" "dns_auth_ext" {
   records = ["${var.ingress_static_ip}"]
 }
 
-resource "aws_route53_record" "dns_app_ext" {
-  zone_id = "${data.aws_route53_zone.ext_zone.zone_id}"
-  name    = "${var.component}.${var.auth_url_prefix}"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["${var.dns_record_addr}"]
-
-  lifecycle {
-    ignore_changes = ["records", "ttl"]
-  }
+output "load_balancer" {
+  value = "${var.ingress_static_ip}"
 }
