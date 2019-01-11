2de7bf55a012cc37b0f1fe704621b207b3f7e85b
Victor Oginskis <victor@agilestacks.com>
2019-01-11 17:41:16 +0200

    GPU instance type list updated

--- k8s-worker-nodes/main.tf
+++ k8s-worker-nodes/main.tf
@@ -18,12 +18,14 @@ data "aws_s3_bucket_object" "bootstrap_script" {
 
 locals {
   gpu_instance_types = [
-    "p2.xlarge",
-    "p2.8xlarge",
-    "p2.16xlarge",
     "p3.2xlarge",
     "p3.8xlarge",
     "p3.16xlarge",
+    "p3dn.24xlarge",
+    "p2.xlarge",
+    "p2.8xlarge",
+    "p2.16xlarge",
+    "g3s.xlarge",
     "g3.4xlarge",
     "g3.8xlarge",
     "g3.16xlarge",
