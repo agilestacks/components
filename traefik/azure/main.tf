8c82dff55e150b98d925e8acebe8d08c6dda2cf5
Arkadi Shishlov <arkadi@agilestacks.com>
2019-04-23 17:29:05 +0300

    Traefik proper Azure resource group name

--- traefik/azure/main.tf
+++ traefik/azure/main.tf
@@ -13,7 +13,7 @@ provider "kubernetes" {
 
 data "azurerm_dns_zone" "ext_zone" {
   name                = "${var.domain_name}"
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "${var.azure_resource_group_name}"
 }
 
 data "kubernetes_service" "traefik" {
@@ -26,7 +26,7 @@ data "kubernetes_service" "traefik" {
 resource "azurerm_dns_a_record" "dns_auth_ext" {
   name                = "${var.auth_url_prefix}"
   zone_name           = "${data.azurerm_dns_zone.ext_zone.name}"
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "${var.azure_resource_group_name}"
   ttl                 = 60
   records             = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.ip}"]
 }
@@ -34,7 +34,7 @@ resource "azurerm_dns_a_record" "dns_auth_ext" {
 resource "azurerm_dns_a_record" "dns_app1_ext" {
   name                = "${var.url_prefix}"
   zone_name           = "${data.azurerm_dns_zone.ext_zone.name}"
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "${var.azure_resource_group_name}"
   ttl                 = 60
   records             = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.ip}"]
 }
@@ -42,7 +42,7 @@ resource "azurerm_dns_a_record" "dns_app1_ext" {
 resource "azurerm_dns_a_record" "dns_app2_ext" {
   name                = "*.${var.url_prefix}"
   zone_name           = "${data.azurerm_dns_zone.ext_zone.name}"
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "${var.azure_resource_group_name}"
   ttl                 = 60
   records             = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.ip}"]
 }
@@ -50,7 +50,7 @@ resource "azurerm_dns_a_record" "dns_app2_ext" {
 resource "azurerm_dns_a_record" "dns_apps1_ext" {
   name                = "${var.sso_url_prefix}"
   zone_name           = "${data.azurerm_dns_zone.ext_zone.name}"
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "${var.azure_resource_group_name}"
   ttl                 = 60
   records             = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.ip}"]
 }
@@ -58,7 +58,7 @@ resource "azurerm_dns_a_record" "dns_apps1_ext" {
 resource "azurerm_dns_a_record" "dns_apps2_ext" {
   name                = "*.${var.sso_url_prefix}"
   zone_name           = "${data.azurerm_dns_zone.ext_zone.name}"
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "${var.azure_resource_group_name}"
   ttl                 = 60
   records             = ["${data.kubernetes_service.traefik.load_balancer_ingress.0.ip}"]
 }
