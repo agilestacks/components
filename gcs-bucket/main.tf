bdaf1d47d1299cf11b36ad1c977f0b53cedb3db2
Arkadi Shishlov <arkadi@agilestacks.com>
2019-05-17 23:15:13 +0300

    GCS bucket component

---
+++ gcs-bucket/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 0.11.10"
+  backend "gcs" {}
+}
+
+provider "google" {
+  project = "${var.project}"
+  version = "2.6.0"
+}
+
+locals {
+  bucket = "${replace(lower(var.name), ".", "-")}"
+}
+
+resource "google_storage_bucket" "main" {
+  name     = "${local.bucket}"
+  location = "${var.location}"
+
+  force_destroy = true
+  versioning {
+    enabled = false
+  }
+}
