514c0599a7030f24c92d721bc1000a92138716bc
Arkadi Shishlov <arkadi@agilestacks.com>
2019-05-21 15:27:07 +0300

    GKE worker nodes `devstorage.read_write` scope

--- gke-worker-nodes/variables.tf
+++ gke-worker-nodes/variables.tf
@@ -11,6 +11,7 @@ variable "asi_oauth_scopes" {
   type = "list"
 
   default = [
+    "https://www.googleapis.com/auth/devstorage.read_write",
     "https://www.googleapis.com/auth/logging.write",
     "https://www.googleapis.com/auth/monitoring",
     "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
