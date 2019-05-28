c778ca71bb76543d685b3adcfbf893e6f27a62c6
Arkadi Shishlov <arkadi@agilestacks.com>
2019-05-28 19:57:01 +0300

    Azure Storage account component

---
+++ azure-storage/main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  version = "1.29.0"
+  alias  = "client"
+
+  subscription_id = "${var.azure_subscription_id}"
+  client_id       = "${var.azure_client_id}"
+  client_secret   = "${var.azure_client_secret}"
+  tenant_id       = "${var.azure_tenant_id}"
+}
+
+locals {
+  account_name_long = "${replace(var.account_name, "/[^[:alnum:]]+/", "")}"
+  account_name = "${substr(local.account_name_long, 0, min(length(local.account_name_long), 24))}"
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "${local.account_name}"
+  location                 = "${var.azure_location}"
+  resource_group_name      = "${var.azure_resource_group_name}"
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "main" {
+  name                  = "${var.container_name}"
+  resource_group_name   = "${var.azure_resource_group_name}"
+  storage_account_name  = "${azurerm_storage_account.main.name}"
+  container_access_type = "private"
+}
---
+++ azure-storage/outputs.tf
@@ -0,0 +1,27 @@
+output "storage_account_name" {
+  value = "${azurerm_storage_account.main.name}"
+}
+
+output "storage_account_region" {
+  value = "${azurerm_storage_account.main.location}"
+}
+
+output "storage_container_name" {
+  value = "${azurerm_storage_container.main.name}"
+}
+
+output "storage_primary_location" {
+  value = "${azurerm_storage_account.main.primary_location}"
+}
+
+output "storage_primary_blob_endpoint" {
+  value = "${azurerm_storage_account.main.primary_blob_endpoint}"
+}
+
+# output "storage_secondary_location" {
+#   value = "${azurerm_storage_account.main.secondary_location}"
+# }
+
+# output "storage_secondary_blob_endpoint" {
+#   value = "${azurerm_storage_account.main.secondary_blob_endpoint}"
+# }
