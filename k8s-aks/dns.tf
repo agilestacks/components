db594954f51c3a958426845182df09c396545f94
Arkadi Shishlov <arkadi@agilestacks.com>
2019-07-05 22:11:19 +0300

    AKS adapter cluster in different resource group?

--- k8s-aks/dns.tf
+++ k8s-aks/dns.tf
@@ -6,6 +6,10 @@ data "azurerm_dns_zone" "base" {
 resource "azurerm_dns_zone" "main" {
   name                = "${var.name}.${data.azurerm_dns_zone.base.name}"
   resource_group_name = "${var.cloud_resource_group_name}"
+  # zone should belong to cluster resource group?
+  # we cannot do this currently as components - such as Traefik and Cert Manager,
+  # won't be able to manipulate DNS records
+  # resource_group_name = "${var.aks_resource_group_name}"
 }
 
 resource "azurerm_dns_ns_record" "parent" {
@@ -20,6 +24,7 @@ resource "azurerm_dns_cname_record" "api" {
   name                = "api"
   zone_name           = "${azurerm_dns_zone.main.name}"
   resource_group_name = "${var.cloud_resource_group_name}"
+  # resource_group_name = "${var.aks_resource_group_name}"
   ttl                 = 60
   record              = "${data.azurerm_kubernetes_cluster.k8s.fqdn}"
 }
