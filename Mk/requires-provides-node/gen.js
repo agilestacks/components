9ace5588b5c979d489e0d16903ac11b41407637e
Arkadi Shishlov <arkadi@agilestacks.com>
2019-07-15 16:31:09 +0300

    `requires` / `provides` meta collector #250

---
+++ Mk/requires-provides-node/gen.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+const {get, keyBy} = require('lodash');
+
+const outputFilename = get(process.argv, '[2]') || 'requires-provides.json';
+const componentsDirectory = get(process.argv, '[3]') || '../../../components';
+
+function write(filename, value) {
+    const out = fs.openSync(filename, 'w');
+    fs.writeFileSync(out, JSON.stringify(value, undefined, 2));
+    fs.closeSync(out);
+}
+
+function extract(components) {
+    return keyBy(
+        components.map(([name, meta]) => ({name, requires: meta.requires || [], provides: meta.provides || []})),
+        'name');
+}
+
+function read(components) {
+    return components.map(([name, filename]) => [name, yaml.safeLoad(fs.readFileSync(filename))]);
+}
+
+function scan(directory) {
+    return fs.readdirSync(directory)
+        .map(name => [name, path.join(directory, name, 'hub-component.yaml')])
+        .filter(([, filename]) => fs.existsSync(filename));
+}
+
+write(outputFilename, extract(read(scan(componentsDirectory))));
