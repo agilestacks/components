34a13d10a386fc37533d57a23ac460c4890ff60d
Oleg Shepetyuk <oleg@agilestacks.com>
2019-08-08 16:51:36 +0300

    EFK: fixed kibana status url

--- efk/conf/post-start-hook.sh
+++ efk/conf/post-start-hook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Creating kibana index."
 
-while [ "$(curl -s -o /dev/null -w "%{http_code}" http://localhost:5601/status)" != "200" ] ; do
+while [ "$(curl -s -o /dev/null -w "%{http_code}" http://localhost:5601/api/status)" != "200" ] ; do
   sleep 10
 done
 curl -f -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: anything" "http://localhost:5601/api/saved_objects/index-pattern/logstash-*" -d "{\"attributes\":{\"title\":\"logstash-*\",\"timeFieldName\":\"@timestamp\"}}"
