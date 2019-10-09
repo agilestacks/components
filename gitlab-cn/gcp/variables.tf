e24de35f7fb66cf874323acc901ae3a0a2f21c1e
Reinis Streics <reinis@agilestacks.com>
2019-10-09 03:09:49 +0300

    #273 added TF support for GCP/Azure

---
+++ gitlab-cn/gcp/variables.tf
@@ -0,0 +1,22 @@
+variable "domain_name" {
+  type = "string"
+  description = "Domain name associated with R53 hosted zone"
+}
+
+variable "kubeconfig_context" {
+  type = "string"
+}
+
+variable "namespace" {
+  type = "string"
+  default = "gitlab"
+}
+
+variable "gitlab_ingress" {
+  type = "string"
+  default = "gitlab-cn-nginx-ingress-controller"
+}
+
+variable "gcp_project_id" {
+  type = "string"
+}
