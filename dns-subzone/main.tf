bbc050b228afb2250ee67018863a27fe0d03d154
Rick Richardson <rick@agilestacks.com>
2020-03-03 18:00:06 -0800

    adding dns-subzone, a standalone component for creating a route53 zone for our cluster agilestacks/automation-hub#778

---
+++ dns-subzone/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_version = ">= 0.11.10"
+  backend "s3" {}
+}
+
+provider "aws" {
+  version = "2.14.0"
+}
+
+# variables
+
+variable "name" {
+  description = "Desired DNS name of the cluster"
+  type        = "string"
+}
+
+variable "base_domain" {
+  description = "DNS base domain"
+  type        = "string"
+}
+
+variable "asi_extra_tags" {
+  type        = "map"
+  default       = {}
+  description = "(optional) Extra tags to be applied to created resources."
+}
+
+data "aws_route53_zone" "base" {
+  name = "${var.base_domain}"
+}
+
+resource "aws_route53_zone" "main" {
+  name          = "${var.name}.${data.aws_route53_zone.base.name}"
+  force_destroy = true
+  tags = "${merge(map(
+      "kubernetes.io/cluster/${var.name}-${var.base_domain}", "owned",
+      "superhub.io/stack/${var.name}.${var.base_domain}", "owned",
+      "Kind", "public",
+    ), var.asi_extra_tags)}"
+}
+
+resource "aws_route53_record" "parent" {
+  zone_id = "${data.aws_route53_zone.base.zone_id}"
+  name    = "${var.name}"
+  type    = "NS"
+  ttl     = "300"
+  records = ["${aws_route53_zone.main.name_servers}"]
+  allow_overwrite = true
+}
