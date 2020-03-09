548f2f34677d2e72cb2633806eb314e184b58167
Victor Oginskis <victor@agilestacks.com>
2020-03-10 01:24:15 +0200

    Choose proper context and namespace

--- service-account/deploy.sh
+++ service-account/deploy.sh
@@ -3,8 +3,10 @@ echo "$ROLE" > role.yaml
 kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" create serviceaccount "$SA"
 kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" apply -f role.yaml
 kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" create rolebinding "$SA"-role-binding --role "$SA"-role --serviceaccount="$NAMESPACE":"$SA"
-SECRET=$(kubectl get serviceaccount "$SA" -o json | jq -r '.secrets[] | select(.name | contains("token")).name')
-TOKEN=$(kubectl get secret "$SECRET" -o json | jq -r '.data.token')
+SECRET=$(kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" \
+  get serviceaccount "$SA" -o json | jq -r '.secrets[] | select(.name | contains("token")).name')
+TOKEN=$(kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" \
+  get secret "$SECRET" -o json | jq -r '.data.token')
 echo
 echo Outputs:
 echo
