651070a3ff3cf2bf4942de73db2a2325f602743e
Arkadi Shishlov <arkadi@agilestacks.com>
2020-04-01 23:10:21 +0300

    PostgreSQLs set `meta.kind`; output `database`, `password`; provides `postgresql`

--- postgresql-rds/outputs.tf
+++ postgresql-rds/outputs.tf
@@ -6,16 +6,16 @@ output "database_security_group_id" {
   value = "${aws_security_group.default.id}"
 }
 
-output "hosted_zone_id" {
-  value = "${aws_db_instance.postgresql.hosted_zone_id}"
-}
-
 output "hostname" {
   value = "${aws_db_instance.postgresql.address}"
 }
 
 output "port" {
   value = "${aws_db_instance.postgresql.port}"
+}
+
+output "name" {
+  value = "${aws_db_instance.postgresql.name}"
 }
 
 output "username" {
