26178d7a922bd662121c64cbc33a5b75e9146b35
Victor Oginskis <victor@agilestacks.com>
2020-04-13 20:14:34 +0300

    Service Account: support cluster roles

--- service-account/deploy.sh
+++ service-account/deploy.sh
@@ -1,13 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 HUB=${HUB:-hub}
 echo "$ROLE" > role.yaml
 kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" create serviceaccount "$SA"
-kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" apply -f role.yaml
-kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" create rolebinding "$SA"-role-binding --role "$SA"-role --serviceaccount="$NAMESPACE":"$SA"
+if [ "$CLUSTER_ROLE" = "true" ]; then
+  kubectl --context="$DOMAIN_NAME" apply -f role.yaml
+  kubectl --context="$DOMAIN_NAME" create clusterrolebinding "$SA"-cluster-role-binding --clusterrole "$SA"-role --serviceaccount="$NAMESPACE":"$SA"
+else
+  kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" apply -f role.yaml
+  kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" create rolebinding "$SA"-role-binding --role "$SA"-role --serviceaccount="$NAMESPACE":"$SA"
+fi
 SECRET=$(kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" \
   get serviceaccount "$SA" -o json | jq -r '.secrets[] | select(.name | contains("token")).name')
 TOKEN=$(kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" \
   get secret "$SECRET" -o json | jq -r '.data.token')
+TOKEN=$(openssl enc -base64 -d -A <<< "$TOKEN")
 echo
 echo Outputs:
 echo
--- service-account/undeploy.sh
+++ service-account/undeploy.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 echo "$ROLE" > role.yaml
-kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" delete rolebinding "$SA"-role-binding
-kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" delete -f role.yaml
+if [ "$CLUSTER_ROLE" = "true" ]; then
+  kubectl --context="$DOMAIN_NAME" delete clusterrolebinding "$SA"-cluster-role-binding
+  kubectl --context="$DOMAIN_NAME" delete -f role.yaml
+else
+  kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" delete rolebinding "$SA"-role-binding
+  kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" delete -f role.yaml
+fi
 kubectl --context="$DOMAIN_NAME" -n "$NAMESPACE" delete serviceaccount "$SA"
