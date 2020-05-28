2c67010dd876879ebba2b46e244d5c91821b8260
Artem Kudla <artem@agilestacks.com>
2020-05-28 19:52:52 +0300

    add spot data feed subsccription to kubecost #416

---
+++ kubecost/main.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">= 0.12.0"
+  backend "s3" {
+  }
+}
+
+provider "aws" {
+  version = "2.49.0"
+}
---
+++ kubecost/variables.tf
@@ -0,0 +1,3 @@
+variable "spot_data_bucket" {}
+variable "spot_data_bucket_region" {}
+variable "spot_data_prefix" {}
