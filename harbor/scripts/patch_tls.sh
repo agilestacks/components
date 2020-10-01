07fa15c05f9cc13f776bb750dda7e423b026f151
Reinis Streics <rstreics@agilestacks.com>
2020-10-01 14:15:17 +0300

    #436 fixed edge case scanrio

--- harbor/scripts/patch_tls.sh
+++ harbor/scripts/patch_tls.sh
@@ -1,19 +1,26 @@
 NAMESPACE=$1
-TIMEOUT=${TIMEOUT:-180}
-POD_SECRET=$(kubectl -n $NAMESPACE get secrets | grep harbor-tls |awk '{print $1}')
-echo "adding ca.crt to '${POD_SECRET}'"
-timeout ${TIMEOUT} bash <<EOT
-    while true; do
-        kubectl patch secret \
+TIMEOUT=${TIMEOUT:-360}
+COUNTER=0
+
+echo "adding ca.crt to harbor"
+while [[ ${COUNTER} -ne ${TIMEOUT} ]]
+do  
+    POD_SECRET=$(kubectl -n $NAMESPACE get secrets | grep harbor-tls |awk '{print $1}')
+    
+    kubectl patch secret \
             -n harbor ${POD_SECRET} \
             -p="{\"data\":{\"ca.crt\": \"$(kubectl get secret \
             -n harbor ${POD_SECRET} \
             -o json -o=jsonpath="{.data.tls\.crt}" \
             | base64 -d | awk 'f;/-----END CERTIFICATE-----/{f=1}' - | openssl enc -A -base64)\"}}" > /dev/null 2>&1
-        if [[ ${?} -eq 0 ]]; then
-            break
-        fi
-        sleep 2
-        printf "."
-    done
-EOT
+    if [[ ${?} -eq 0 ]]; then
+        echo "Patched harbor secret"
+        exit 0
+    fi
+
+    let COUNTER=COUNTER+1
+    sleep 2
+done
+
+echo "failed to patch harbor secret"
+exit 1
